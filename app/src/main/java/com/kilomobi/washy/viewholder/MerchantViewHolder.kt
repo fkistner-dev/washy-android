5743ff76c042d6ffdc191118dee282b7b8699112
Fabrice KISTNER <fabrice.kistner.pro@gmail.com>
2023-04-19 18:43:34 +0200

    add pro chip to merchant

--- app/src/main/java/com/kilomobi/washy/viewholder/MerchantViewHolder.kt
+++ app/src/main/java/com/kilomobi/washy/viewholder/MerchantViewHolder.kt
@@ -24,17 +24,19 @@ class MerchantViewHolder(private val merchantView: View) : RecyclerView.ViewHold
     val subtext: Int = R.id.subtext
     val rating: Int = R.id.ratingBar
     val cardview: Int = R.id.cardview
+    val professionnal: Int = R.id.professional
     private val horizontalScroll: Int = R.id.serviceHorizontalScroll
 
     fun bind(merchant: Merchant, selectedItem: Int) {
         val context = merchantView.context
         if (merchant.imported) {
             merchantView.findViewById<TextView>(header).visibility = View.GONE
-        } else {
-            merchantView.findViewById<TextView>(header).text = if (merchant.siren?.isNotEmpty() == true) context.getString(R.string.merchant_pro) else context.getString(R.string.merchant_part)
+        } else if (merchant.siren?.isNotEmpty() == true) {
+            merchantView.findViewById<Chip>(professionnal).visibility = View.VISIBLE
         }
 
-        merchantView.findViewById<TextView>(title).text = merchant.name
+        merchantView.findViewById<TextView>(header).text = merchant.name
+        merchantView.findViewById<TextView>(title).text = merchant.geohash // don't hide it, or chip will have no anchors
         merchantView.findViewById<TextView>(text).textOrHide(merchant.description)
         merchantView.findViewById<TextView>(subtext).textOrHide(merchant.fullAddress)
         merchantView.findViewById<MaterialRatingBar>(rating).rating = merchant.avgRating
